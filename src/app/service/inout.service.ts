f755bcc69d59a9db0cb6219f5e08501bf87a6819
originalstacie <15928974167@163.com>
2020-07-14 17:51:40 +0800

    修改操作指导bug

--- src/app/service/inout.service.ts
+++ src/app/service/inout.service.ts
@@ -1,42 +1,48 @@
-import { Injectable } from '@angular/core';
-import { HttpHeaders, HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core'
+import { HttpHeaders, HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
 
-const headers = new HttpHeaders({
-  'Content-Type': 'application/json;charset=utf-8',
-  'token': localStorage.getItem('token')
-});
+// const headers = new HttpHeaders({
+//     'Content-Type': 'application/json;charset=utf-8',
+//     token: localStorage.getItem('token'),
+// })
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class InoutService {
-  excuteUrl = 'http://47.114.39.156:3300/api/web/v1/';
-  constructor(private http: HttpClient) { }
+    excuteUrl = 'http://47.114.39.156:3300/api/web/v1/'
+    constructor(private http: HttpClient) {}
 
-  post(url: string, body: any): Observable<any> {
-    return this.http.post<any>(this.excuteUrl + url, body, {
-      headers,
-      responseType: 'json'
-    })
-  }
-
-  getToken(url: string, body: any): Observable<any> {
-    return this.http.get<any>(this.excuteUrl + url + this.buildparams(body), {
-      headers,
-      responseType: 'json'
-    })
-  }
-
-  buildparams(data): string {
-    if (Object.keys(data).length) {
-      let myary: string[] = []
-      Object.keys(data).forEach((item) => {
-        myary.push(item + '=' + data[item])
-      })
-      return '?' + myary.join('&')
-    } else {
-      return ''
+    post(url: string, body: any): Observable<any> {
+        return this.http.post<any>(this.excuteUrl + url, body, this.createOptions())
     }
-  }
+
+    getToken(url: string, body: any): Observable<any> {
+        return this.http.get<any>(this.excuteUrl + url + this.buildparams(body), this.createOptions())
+    }
+
+    buildparams(data): string {
+        if (Object.keys(data).length) {
+            let myary: string[] = []
+            Object.keys(data).forEach(item => {
+                myary.push(item + '=' + data[item])
+            })
+            return '?' + myary.join('&')
+        } else {
+            return ''
+        }
+    }
+
+    private createOptions() {
+        let headers = new HttpHeaders({
+            'Content-Type': 'application/json;charset=utf-8',
+            token: localStorage.getItem('token'),
+        })
+
+        let options = {
+            headers: headers,
+        }
+        return options
+    }
 }
